6b8f727db5be8a3e3b88b6f455c5dff89c618791
Kurt <kg3@users.noreply.github.com>
2016-04-30 13:46:39 -0500

    Update mortars.py

--- mortars.py
+++ mortars.py
@@ -9,7 +9,7 @@ from visual.graph import *
 from random import *
 
 ## Functions #### Functions #### Functions ##
-def credit(Theta,wTheta, Vi,BLDG1,BLDG2) :
+def credit(Theta,Vi,BLDG1,BLDG2) :
     print "Mortar Anatomy by Kurt Gibbons\n",
     print "For Physics 2425 Fall 2013 Class Presentation\n",
     print "Professor: Jason Eberle\n",
@@ -19,12 +19,8 @@ def credit(Theta,wTheta, Vi,BLDG1,BLDG2) :
     C = Pathagorean(dX,dY)
     print "Distances = X: %.1f Y: %.1f Z: %.1f" % (dX,dY,dZ)
     print "Total Distance Between Buildings = %.1f" % (C)
-    bestTheta1,bestTheta2,InitVel,wV,ADT1,ADT2 = Solver(BLDG1, BLDG2, Vi, Theta, wTheta)
+    bestTheta1,bestTheta2 = Solver(BLDG1, BLDG2, Vi, Theta)
     print "Theta =  %.1f deg or %.1f deg" % (bestTheta1,bestTheta2)
-    print "Initial Velocity = %.1f m/s" % (InitVel)
-    print "Air Drag Initial Velocity = %.1f m/s" % (wV)
-    print "Air Drag Theta =  %.1f deg or %.1f deg" % (ADT1,ADT2)
-    
     
 def MainWindow(X,Y,Z,Title,fgcolor,bgcolor) :
     #  Input: X,Y,Z (Range of view)
@@ -176,7 +172,7 @@ def Pathagorean(X,Y) :
     #Purpose: Caclulate hypotenus
     return sqrt(X**2+Y**2)
 
-def Solver(pointA, pointB, Vi, Theta, wTheta):
+def Solver(pointA, pointB, Vi, Theta):
     #  Input: bldg1 & bldg2 positions
     # Output: Correct Initial Velocity & Angle
     #Purpose: Solve for the correct Inputs
@@ -184,11 +180,7 @@ def Solver(pointA, pointB, Vi, Theta, wTheta):
     C = Pathagorean(dX,dY)
     BestTheta1 = degrees((.5)*asin( (9.8*Range(Vi))/float(Vi**2) ))
     BestTheta2 = (180-BestTheta1) / 2
-    InitialVelocity = sqrt( G*(float(C)/sin(2*radians(Theta))) )
-    ADVi = sqrt( G*(float(C)/sin(2*radians(wTheta))) )
-    ADTheta1 = degrees((.5)*asin( (9.8*Range(ADVi))/float(ADVi**2) ))
-    ADTheta2 = (180-BestTheta1) / 2
-    return BestTheta1, BestTheta2, InitialVelocity,ADVi,ADTheta1, ADTheta2
+    return BestTheta1, BestTheta2
     
 #~#~#~# Main #~#~#~# #~#~#~# Main #~#~#~# #~#~#~# Main #~#~#~#
 #Projectile Variables
@@ -197,19 +189,15 @@ dt = 0.025               # change in time
 G = 9.8                 # m/s**2
 t = 0                   # Time
 Vi = 51                 # m/s (Initial Velocity)        
-Theta = 83.8
+Theta = 77.5
 ThetaRad = radians(Theta)     # degrees (Launch Angle Theta)
-Theta2 = ThetaRad
 # Red Mortar
 Direction1 = radians(1)  # Adjustment for aiming in degrees   
 # Green Mortar
 Direction2 = -Direction1 # Adjustment for aiming in degrees
 
-VelocityX1 = Vi*cos(Theta2)
-VelocityY1 = Vi*sin(Theta2)
-
-VelocityX2 = Vi*cos(ThetaRad)
-VelocityY2 = Vi*sin(ThetaRad)
+VelocityX = Vi*cos(ThetaRad)
+VelocityY = Vi*sin(ThetaRad)
 
 
 m = 2.01                #0.145  # mass of 60mm is 2.01 kg, 120mm is 13.65 kg
@@ -220,17 +208,17 @@ D = (rho*C*A)/2
 g = vector(0,-9.8,0)    # gravity vector
 
 # WIND MORTARS
-# W = -3 #-3                  # m/s (negative towards screen, positive towards viewer)
-# Wind = vector(0,0,W)    # wind vector
-wVi = 51                # m/s (Initial Velocity)        
-wTheta = 83.8
+W = -3 #-3                  # m/s (negative towards screen, positive towards viewer)
+Wind = vector(0,0,W)    # wind vector
+wVi = 53                 # m/s (Initial Velocity)        
+wTheta = 77.5
 wThetaRad = radians(wTheta)     # degrees (Launch Angle Theta)
 wVelocityX = wVi*cos(wThetaRad)
 wVelocityY = wVi*sin(wThetaRad)
 # Red Mortar
-wDirection1 = radians(.5)  # Adjustment for aiming in degrees then rads
+wDirection1 = radians(12)  # Adjustment for aiming in degrees then rads
 # Green Mortar
-#wDirection2 = radians(-12) # Adjustment for aiming in degrees
+wDirection2 = radians(200) # Adjustment for aiming in degrees
 
 
 # Screen Variables
@@ -250,7 +238,7 @@ bldg1a = rectangle2stories(color.red,floorW,floorPosY,ran1,width,randint(1, 3),1
 bldg2  = rectangle2stories(color.green,floorW,floorPosY,ran2,width,randint(3, 5),1,'right')
 bldg2a = rectangle2stories(color.green,floorW,floorPosY,ran2,width,randint(1, 3),1+random(),'right')
 
-credit(Theta,wTheta,Vi,bldg1a.pos,bldg2a.pos)
+credit(Theta,Vi,bldg1a.pos,bldg2a.pos)
 
 # Projectile Graphs
 gh,gw = 190, 570    # graph height, graph width
@@ -264,24 +252,26 @@ Mortar2Graph, Mortar2Graph_plot = Graphs(0,570,gw,gh,'Green Shooter Position & V
 #  Input: Radius, BallPos X,Y,Z, Colors, Vector Vx, Vy, Vz 
 p1ball,p1ball.trail,p1ball.velocity,p1ball.acceleration = ProjectileBall(.5,bldg1a.pos.x,bldg1a.pos.y,bldg1a.pos.z,
                                                                     color.red,color.blue,
-                                                                    VelocityX1,VelocityY1,0,
+                                                                    VelocityX,VelocityY,0,
                                                                     0,-G,0)
 p2ball,p2ball.trail,p2ball.velocity,p2ball.acceleration = ProjectileBall(.5,bldg2a.pos.x,bldg2a.pos.y,bldg2a.pos.z,
                                                                     color.green,color.blue,
-                                                                    -VelocityX2,VelocityY2,0,
+                                                                    -VelocityX,VelocityY,0,
                                                                     0,-G,0)
 W1ball,W1ball.trail,W1ball.velocity,W1ball.acceleration = ProjectileBall(.5,bldg1a.pos.x,bldg1a.pos.y,bldg1a.pos.z,
                                                                                 color.white,color.white,
                                                                                 wVelocityX,wVelocityY,0,
                                                                                 0,-G,0)
-# W2ball,W2ball.trail,W2ball.velocity,W2ball.acceleration = ProjectileBall(.5,bldg2a.pos.x,bldg2a.pos.y,bldg2a.pos.z,
-#                                                                                 color.white,color.white,
-#                                                                                 wVelocityX,wVelocityY,0,
-#                                                                                 0,-G,0)                                                                                
+W2ball,W2ball.trail,W2ball.velocity,W2ball.acceleration = ProjectileBall(.5,bldg2a.pos.x,bldg2a.pos.y,bldg2a.pos.z,
+                                                                                color.white,color.white,
+                                                                                wVelocityX,wVelocityY,0,
+                                                                                0,-G,0)                                                                                
 
 Xlab,Y1lab,Y2lab = -50,40,20    # Position of labels
 
-PresentWith = 'Wind'        # Select to show Wind or not, All, Wind, NoWind
+Range(Vi)
+
+PresentWith = 'All'        # Select to show Wind or not, All, Wind, NoWind
 scene.mouse.getclick()      # wait for mouse click
     
 if PresentWith == 'NoWind' :
@@ -294,7 +284,7 @@ if PresentWith == 'NoWind' :
         # Red Mortar
         p1ball.pos += vector(0,0,Direction1)+p1ball.velocity*dt + 0.5 * p1ball.acceleration*dt**2
         p1ball.velocity += p1ball.acceleration*dt
-        p1ball.acceleration = g
+        p1ball.acceleration = g*m
         p1ball.trail.append( pos = p1ball.pos )
         Mortar1Pos_plot.plot(pos =(t,p1ball.pos.y))
         Mortar1Graph_plot.plot(pos =(p1ball.velocity.y,p1ball.pos.y))
@@ -304,7 +294,7 @@ if PresentWith == 'NoWind' :
         # Green Mortar
         p2ball.pos += vector(0,0,Direction2)+p2ball.velocity*dt + 0.5 * p2ball.acceleration*dt**2
         p2ball.velocity += p2ball.acceleration*dt
-        p2ball.acceleration = g
+        p2ball.acceleration = g*m
         p2ball.trail.append( pos = p2ball.pos )
         Mortar2Pos_plot.plot(pos =(t,p2ball.pos.y))
         Mortar2Graph_plot.plot(pos =(p2ball.velocity.y,p2ball.pos.y))
@@ -315,7 +305,7 @@ if PresentWith == 'NoWind' :
                                 Vi,Theta,t)
 
 elif PresentWith == 'Wind' :
-    while (W1ball.pos.y) > (floorPosY + 1) :
+    while (W1ball.pos.y and W2ball.pos.y) > (floorPosY + 1) :
         if scene.mouse.clicked:             # Control play with mouse click
             scene.mouse.getclick()
             scene.mouse.getclick() 
@@ -325,22 +315,22 @@ elif PresentWith == 'Wind' :
         # Red Wind Mortar
         W1ball.pos += vector(0,0,wDirection1)+W1ball.velocity*dt + 0.5 * W1ball.acceleration*dt**2
         W1ball.velocity += W1ball.acceleration*dt
-        W1ball.acceleration = g -(D/m)*mag(W1ball.velocity)*W1ball.velocity
+        W1ball.acceleration = g*m -(D/m)*mag(W1ball.velocity)*W1ball.velocity + Wind
         W1ball.trail.append( pos = W1ball.pos )
         Mortar1Pos_plot.plot(pos =(t,W1ball.pos.y))
         Mortar1Graph_plot.plot(pos =(W1ball.velocity.y,W1ball.pos.y))
         M1Label = BoxLabel( color.red,Xlab,Y1lab,W1ball.velocity,W1ball.pos,
                             W1ball.pos.x-bldg1a.pos.x, 'Red Wind Mortar' )
         
-#         #Green Wind Mortar
-#         W2ball.pos += vector(0,0,wDirection2)+W2ball.velocity*dt + 0.5 * W2ball.acceleration*dt**2
-#         W2ball.velocity += W2ball.acceleration*dt
-#         W2ball.acceleration = g*m -(D/m)*mag(W2ball.velocity)*W2ball.velocity + Wind
-#         W2ball.trail.append( pos = W2ball.pos )
-#         Mortar2Pos_plot.plot(pos =(t,W2ball.pos.y))
-#         Mortar2Graph_plot.plot(pos =(W2ball.velocity.y,W2ball.pos.y))
-#         M2Label = BoxLabel( color.green,Xlab,Y2lab,W2ball.velocity,W2ball.pos,
-#                             W2ball.pos.x-bldg2a.pos.x, 'Green Wind Mortar' )
+        #Green Wind Mortar
+        W2ball.pos += vector(0,0,wDirection2)+W2ball.velocity*dt + 0.5 * W2ball.acceleration*dt**2
+        W2ball.velocity += W2ball.acceleration*dt
+        W2ball.acceleration = g*m -(D/m)*mag(W2ball.velocity)*W2ball.velocity + Wind
+        W2ball.trail.append( pos = W2ball.pos )
+        Mortar2Pos_plot.plot(pos =(t,W2ball.pos.y))
+        Mortar2Graph_plot.plot(pos =(W2ball.velocity.y,W2ball.pos.y))
+        M2Label = BoxLabel( color.green,Xlab,Y2lab,W2ball.velocity,W2ball.pos,
+                            W2ball.pos.x-bldg2a.pos.x, 'Green Wind Mortar' )
         
         Readings = BottomLabel(-scene.range.x/4,-scene.range.y/2,0,(bldg1a.pos.x-bldg2a.pos.x),
                                 wVi,wTheta,t)
@@ -356,7 +346,7 @@ elif PresentWith == 'All' :
         # Red Mortar
         p1ball.pos += vector(0,0,Direction1)+p1ball.velocity*dt + 0.5 * p1ball.acceleration*dt**2
         p1ball.velocity += p1ball.acceleration*dt
-        p1ball.acceleration = g
+        p1ball.acceleration = g*m
         p1ball.trail.append( pos = p1ball.pos )
         Mortar1Pos_plot.plot(pos =(t,p1ball.pos.y))
         Mortar1Graph_plot.plot(pos =(p1ball.velocity.y,p1ball.pos.y))
@@ -364,22 +354,22 @@ elif PresentWith == 'All' :
         # Green Mortar
         p2ball.pos += vector(0,0,Direction2)+p2ball.velocity*dt + 0.5 * p2ball.acceleration*dt**2
         p2ball.velocity += p2ball.acceleration*dt
-        p2ball.acceleration = g
+        p2ball.acceleration = g*m
         p2ball.trail.append( pos = p2ball.pos )
     
         # Red Wind Mortar
         W1ball.pos += vector(0,0,wDirection1)+W1ball.velocity*dt + 0.5 * W1ball.acceleration*dt**2
         W1ball.velocity += W1ball.acceleration*dt
-        W1ball.acceleration = g -(D/m)*mag(W1ball.velocity)*W1ball.velocity 
+        W1ball.acceleration = g*m -(D/m)*mag(W1ball.velocity)*W1ball.velocity + Wind
         W1ball.trail.append( pos = W1ball.pos )
         
-#         #Green Wind Mortar
-#         W2ball.pos += vector(0,0,wDirection2)+W2ball.velocity*dt + 0.5 * W2ball.acceleration*dt**2
-#         W2ball.velocity += W2ball.acceleration*dt
-#         W2ball.acceleration = g -(D/m)*mag(W2ball.velocity)*W2ball.velocity 
-#         W2ball.trail.append( pos = W2ball.pos )
-#         Mortar2Pos_plot.plot(pos =(t,W2ball.pos.y))
-#         Mortar2Graph_plot.plot(pos =(W2ball.velocity.y,W2ball.pos.y))
+        #Green Wind Mortar
+        W2ball.pos += vector(0,0,wDirection2)+W2ball.velocity*dt + 0.5 * W2ball.acceleration*dt**2
+        W2ball.velocity += W2ball.acceleration*dt
+        W2ball.acceleration = g*m -(D/m)*mag(W2ball.velocity)*W2ball.velocity + Wind
+        W2ball.trail.append( pos = W2ball.pos )
+        Mortar2Pos_plot.plot(pos =(t,W2ball.pos.y))
+        Mortar2Graph_plot.plot(pos =(W2ball.velocity.y,W2ball.pos.y))
        
         Readings = AllBottomLabel(-scene.range.x/4,-scene.range.y/2,0,(bldg1a.pos.x-bldg2a.pos.x),
                                 Vi,Theta,wVi,wTheta,t)
